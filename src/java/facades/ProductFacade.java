f4143bda50a8f1d8f7945de2e7d7599fd3ca2a6b
pupil <pupil@ivkhk.ee>
2022-05-25 11:16:32 +0300

    Start

---
+++ src/java/facades/ProductFacade.java
@@ -0,0 +1,24 @@
+
+package facades;
+
+import enitys.Product;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class ProductFacade extends AbstractFacade<Product> {
+
+    @PersistenceContext(unitName = "WebBootsShopJSPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ProductFacade() {
+        super(Product.class);
+    }
+
+}
